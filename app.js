84b1b02579a404541b848860aca5ce551351e650
Raj <kkusanjal@gmail.com>
2023-05-04 18:45:27 +0530

    changing the server

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 require("dotenv").config()
 const express = require('express');
 const app = express();
+var http= require("http").Server(app);
 const port = process.env.PORT
 var path = require("path");
+var io = require("socket.io")(http);
 
 //middle are 
 app.use(express.json());
@@ -16,10 +18,17 @@ app.get("/",function(req,res){
     var fileName = "index.html";
     res.sendFile(fileName,options);
 });
-//creating the server at given port 
-app.listen(port,(req,res)=>{
-    console.log(`Listening at port ${port}`);
+ 
+io.on('connection',function(socket){
+    console.log("A user Connected");
+    socket.on("disconnect",function(){
+        console.log("All user Disconnected");
+    })
 })
+//creating the server at given port 
+http.listen(port,()=>{
+    console.log(`Listening at port ${port}`);
+});
 
 
 
